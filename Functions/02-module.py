5fdf13c14080f2c979f5bacd296736800e115aa8
venkatparsapu123@gmail.com <venkatparsapu123@gmail.com>
2025-02-16 19:25:19 +0530

    datatypes

---
+++ Functions/02-module.py
@@ -0,0 +1,4 @@
+import calc_new
+
+print(calc_new.addition(10,15))
+print(calc_new.subtraction(20,5))
---
+++ Functions/calc_new.py
@@ -0,0 +1,16 @@
+num1=10
+num2=5
+def addition(num1,num2):
+    c=num1+num2
+    return c
+
+print (addition(20,25))
+
+def subtraction(num1,num2):
+    sub=num1-num2
+    return sub
+
+print(subtraction(10,15))
+
+
+
